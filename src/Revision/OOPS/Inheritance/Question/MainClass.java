12521d2231b57fdfde9e58a563145c8bc8c4231f
anurag <anurag3056@gmail.com>
2025-11-07 17:56:20 +0530

    InheritanceComments

--- src/Revision/OOPS/Inheritance/Question/MainClass.java
+++ src/Revision/OOPS/Inheritance/Question/MainClass.java
@@ -5,8 +5,10 @@ public class MainClass {
     public static void main(String[] args)
     {
         ClassA obj1 = new ClassB();// obj has class A reference and classB also extend ClassA
-        System.out.println(obj1.i);// i will have classA value
-        obj1.display1();
+
+
+        System.out.println(obj1.i);// i will have classA value, variable depends on left hand side which has been referenced
+        obj1.display1();// method of classB will be called ,method depends on right hand side, whose object created
 
         ClassB obj2 = new ClassB();
         System.out.println(obj2.i);// i will have classB value
